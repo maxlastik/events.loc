05a9b855fcaecb23ebcf38dfa036e5a8da261008
Maxim Korenev <fudgemax007@gmail.com>
2024-07-02 15:17:15 +0300

    1. Image Faker has been changed for "LoremFlickr". 2. Readme file has been updated.

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 Just Pet Project - Simple Events Ticketing System.
 
+Installation:
 1) git clone https://github.com/maxlastik/events.loc.git
-2) composer install
-3) copy .env.example .env
+2) copy .env.example .env
+3) composer install
 4) php artisan key:generate
-5) touch database/database.sqlite
-6) php artisan storage:link
-7) php artisan migrate:refresh --seed
+5) php artisan storage:link
+6) php artisan migrate:refresh --seed
+
+---------------
+Admin Account : admin@admin.ru / secret
+
+Member Account: member@member.ru / secret
+
+All emails (for account verification) will be placed in laravel.log file.
--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Alirezasedghi\LaravelImageFaker\ImageFaker;
-use Alirezasedghi\LaravelImageFaker\Services\Picsum;
+use Alirezasedghi\LaravelImageFaker\Services\LoremFlickr;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -21,12 +21,11 @@ class EventFactory extends Factory
         $start_date = $this->faker->dateTimeBetween('next Monday', 'next Monday +7 days');
         $end_date = $this->faker->dateTimeBetween($start_date, $start_date->format('Y-m-d H:i:s').' +2 days');
 
-        $imageFaker = new ImageFaker(new Picsum());
+        $imageFaker = new ImageFaker(new LoremFlickr());
 
         return [
             'title' => $this->faker->sentence(),
             'image' => $imageFaker->image(storage_path('app/public/images/events'),640,480,false,false,true),
-            //'image' => $this->faker->image(storage_path('app/public/images/events'), 300, 300, null, false),
             'description' => $this->faker->realText,
             'start_date' => $start_date,
             'end_date' => $end_date,
